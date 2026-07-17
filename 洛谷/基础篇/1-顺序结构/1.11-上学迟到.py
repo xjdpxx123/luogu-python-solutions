cab29705de4e02a16b35d5e1301ef1935bf2fe7f
xjdpxx <xjdpxx123@users.noreply.github.com>
2026-07-17 10:02:03 +0800

    renew

---
+++ 洛谷/基础篇/1-顺序结构/1.11-上学迟到.py
@@ -0,0 +1,15 @@
+s,v = map(int, input().split())
+
+# walk_time = (s+v-1)//v，适用于整数
+walk_time = s // v
+if s%v != 0:
+    walk_time += 1
+
+total_time = walk_time + 10
+
+leave_time = (8*60 - total_time) % (24*60)
+
+hour = leave_time // 60
+minute = leave_time % 60
+
+print(f"{hour:02d}:{minute:02d}")
